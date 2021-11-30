f894eb9b2111887438b916c20a2d3e97a5977b98
Ana <contatodeanasilva@gmail.com>
2021-11-30 20:37:23 -0300

    Tela login

--- app/src/main/java/com/example/projetolojavirtual/MainActivity.java
+++ app/src/main/java/com/example/projetolojavirtual/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.projetolojavirtual;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,4 +14,25 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void logar(View v){
+
+        EditText tusuario = (EditText) findViewById(R.id.edtTextUsuario);
+        EditText tsenha = (EditText) findViewById(R.id.edtTextSenha);
+
+        String usuario = tusuario.getText().toString();
+        String senha = tsenha.getText().toString();
+
+        if(usuario.equals("admin") && senha.equals("admin")){
+            alert("Login realizado com sucesso!");
+        }
+        else{
+            alert("Usuário ou senha incorretos");
+        }
+
+    }
+    public void alert(String s){
+        Toast.makeText(this, "s", Toast.LENGTH_SHORT).show();
+    }
+
 }
